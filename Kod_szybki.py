4c57fa3e8bd9c267ff27b2c8dc3f91a710f9ec8f
Olga Sieradzan <sieradzan@student.agh.edu.pl>
2024-08-29 17:39:21 +0200

    rzowiązanie una problema

--- Kod_szybki.py
+++ Kod_szybki.py
@@ -54,15 +54,56 @@ def date_max_turnovers(df, turn, window): # argimenty to : pierwotna tabela/ tal
     return result
 
 
+def rate_of_return(df, turn, investment, window): # argumenty : ramka z cenami zamknięcia, ramka z obrót/obrót max zwrotami, okres inwestycji, okres referencyjny
+    result = pd.DataFrame(columns=df.columns)
+
+    for col in df.columns:
+        if col == 'Date':
+            result['Date'] = df['Date'][window+investment:].reset_index(drop=True) # przesuwamy daty o okres inwestycji ponieważ wtedy stopy zwrotu zapiszą się w ostatni dzień inwestycji 
+        else:
+            return_values = []
+            for i in range(len(df)- window):
+                if pd.isna(turn[col].iloc[i]):
+                    return_values.append(np.nan)
+                elif not pd.isna(turn[col].iloc[i]) and i + investment <= len(df) and not pd.isna(df[col].iloc[i]) and not pd.isna(df[col].iloc[i + investment]):
+                    return_values.append((df[col].iloc[i + investment] - df[col].iloc[i])/df[col].iloc[i]) 
+            result[col] = pd.Series(return_values).reset_index(drop=True)
+  
+    return result
+
+
 
 ## RAMKI ##
 
+
+
 prices = pd.read_excel(file_path1)
 turnovers = pd.read_excel(file_path2)
+
 prices_cleaned, turnovers_cleaned = clean_data(prices, 'KGH:PL') # Zapisane wyników ( ubyło 209 wierszy )
+
+prices_cleaned.iloc[:, 1:] = prices_cleaned.iloc[:, 1:].applymap(lambda x: pd.to_numeric(x, errors='coerce'))
+
 turnovers_20 = max_turnover(turnovers_cleaned, 20)
 turnovers_10 = max_turnover(turnovers_cleaned, 10)
 turnovers_50 = max_turnover(turnovers_cleaned, 50)
+
 result_20 = date_max_turnovers(turnovers_cleaned, turnovers_20, 20)
 result_10 = date_max_turnovers(turnovers_cleaned, turnovers_10, 10)
 result_50 = date_max_turnovers(turnovers_cleaned, turnovers_50, 50)
+
+rates_10_5 = rate_of_return(prices_cleaned, result_10, 1, 10)
+rates_10_5 = rate_of_return(prices_cleaned, result_20, 1, 20)
+rates_10_5 = rate_of_return(prices_cleaned, result_50, 1, 50)
+
+rates_10_5 = rate_of_return(prices_cleaned, result_10, 2, 10)
+rates_10_5 = rate_of_return(prices_cleaned, result_20, 2, 20)
+rates_10_5 = rate_of_return(prices_cleaned, result_50, 2, 50)
+
+rates_10_5 = rate_of_return(prices_cleaned, result_10, 3, 10)
+rates_10_5 = rate_of_return(prices_cleaned, result_20, 3, 20)
+rates_10_5 = rate_of_return(prices_cleaned, result_50, 3, 50)
+
+rates_10_5 = rate_of_return(prices_cleaned, result_10, 10, 10)
+rates_10_5 = rate_of_return(prices_cleaned, result_20, 10, 20)
+rates_10_5 = rate_of_return(prices_cleaned, result_50, 10, 50)
